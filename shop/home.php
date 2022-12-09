90958dbb7b516d17285cc03b5e7a7eb588c55542
Eilaf7 <79628248+Eilaf7@users.noreply.github.com>
2022-12-09 14:32:13 +0400

    Add files via upload

---
+++ shop/home.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html  dir="rtl">
+   <head>
+
+<title>Candy shop</title>
+<meta charset="utf-8">
+<meta name="discription" content="CANDY SHOP">
+<link rel="stylesheet" href="css/stylei.css">
+        </head>
+       <body>
+            <header>
+             <div class= "container">
+                 <div id= "brand">
+                     <h1> Sweet Candy   </h1>
+                     
+
+
+                </div>
+                        
+                  <nav class="navbar">
+                  
+                         <a href="login.php"> |  login |  </a></li>
+
+
+                            
+
+
+
+                        
+                     
+
+                  </nav>
+
+          </div>
+
+
+</header>
+<section id= "cass">
+   
+</section>
+   <section id= "newsletter" >
+      <div class= "container">
+         <h1>  إشترك ليصلك كل جديد   </h1>
+         <form action="">
+
+      </div action="">
+      
+<input type="email" placeholder="أدخل  بريدك الإلكتروني ">
+      <button type="submit" class="button">اشترك</button>
+              </form>
+              </div>
+             </section>
+             <section id="boxes">
+                 
+                 <div class="content">
+                    <div class="box">
+                               <p>
+                                 <img src="img/rr.jpg" width="200px">
+
+                              </p>
+                                 <h2>أجمل النكهات </h2>
+                          </div>
+                    <div class="box">
+                              <p>
+                                <img src="img/bth.jpg" width="200px">
+                             </p>
+                              <h3>معكم في مناسباتكم واعيادكم</h3>
+                    </div>
+                    <div class="box">
+                             <p>
+
+                                <img src="img/clook.jpg" width="200px">
+                            </p>
+                                <h2>تسليم بالوقت المحدد</h2>
+                    </div>
+                 </div>
+            
+                
+             </section>
+
+              </section>
+</body>
+   <footer>
+       <p>  <h2>متجرنا الإلكتروني للحلويات </h2>&COPY;2022</p>
+
+   </footer>
+
+</html>
